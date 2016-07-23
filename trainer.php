6bc0db0ecf9c8cf4b6f8b0c47cac68fa2c2126d3
Chanala Wilangka <c.wilangka@gmail.com>
2016-07-23 15:28:50 +0700

    trainer

--- trainer.php
+++ trainer.php
@@ -32,26 +32,24 @@ include('header.php');
                         <p class="text_desc">นักวางแผนการเงิน, CEO และผู้ก่อตั้ง บริษัท CMSK Wealth Management จำกัด</p>
                         <p class="text_detail">จบปริญญาเอกด้านวิศวไฟฟ้า จาก University of Southern California (USC) แต่ด้วยความสนใจ ด้านการเงินส่วนบุคคลและการทำธุรกิจ ดร.ชาติชาย ผ่านการรับรอง Certified Financial Planner (CFP) ซึ่งเป็นประกาศนียบัตรสำหรับนักวางแผนการเงิน ระดับสากลที่ได้รับการเชื่อถือมากที่สุด รวมถึงก่อตั้ง บริษัท CMSK จำกัด โดยมีวัตถุประสงค์เพื่อพัฒนา เป็น World Class Wealth Management Firm ปัจจุบัน ดร.ชาติชาย เป็น CEO และ นักวางแผน การเงินของ CMSK และวิทยากรให้กับสถาบัน การเงินชั้นนำหลายแห่ง และอยากพัฒนาบริการ Wealth Management ให้มีประสิทธิภาพสูงสุด กับคนทุกระดับ
                         </p>
+                        <div class="space_top"></div>
+                        <p class="text_title">ประวัติการศึกษา</p>
+                        <ul class="text_list">
+                            <li>ปริญญาตรี วิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย</li>
+                            <li>ปริญญาโท Electrical Engineering, University of Southern California, USA</li>
+                            <li>ปริญญาเอก Electrical Engineering, University of Southern California, USA</li>
+                        </ul>
+                       <div class="space_top"></div>
+                        <p class="text_title">ประสบการณ์การทำงาน</p>
+                        <ul class="text_list">
+                            <li>Certified Financial Planner (CFP), บริษัท CMSK จำกัด</li>
+                            <li>ตลาดการเงินระหว่างประเทศ</li>
+                            <li>วิทยากร สมาคมบริษัทจัดการลงทุน (AIMC) หลักสูตรผู้แนะนำการลงทุนด้านหลักทรัพย์ (Investor Contact) และ Financial Planning</li>
+                        </ul>
                     </div>
                 </div>
                 <!-- end trainer_right -->
                 <div class="trainer_block">
-                    <br>
-                    <p class="text_title">ประวัติการศึกษา</p>
-                    <ul class="text_list">
-                        <li>ปริญญาตรี วิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย</li>
-                        <li>ปริญญาโท Electrical Engineering, University of Southern California, USA</li>
-                        <li>ปริญญาเอก Electrical Engineering, University of Southern California, USA</li>
-                    </ul>
-                </div>
-                <div class="trainer_block">
-                    <br>
-                    <p class="text_title">ประสบการณ์การทำงาน</p>
-                    <ul class="text_list">
-                        <li>Certified Financial Planner (CFP), บริษัท CMSK จำกัด</li>
-                        <li>ตลาดการเงินระหว่างประเทศ</li>
-                        <li>วิทยากร สมาคมบริษัทจัดการลงทุน (AIMC) หลักสูตรผู้แนะนำการลงทุนด้านหลักทรัพย์ (Investor Contact) และ Financial Planning</li>
-                    </ul>
                 </div>
             </div>
             <!-- end trainer_wrapper -->
